fb320e8012639dddf927e35aa1d94471b2ecb703
Jaisen Mathai <jmathai@joythrown-lm.corp.yahoo.com>
2008-02-13 10:41:00 -0800

    Tweaked layout/css for the date display.

--- html/blog/wp-content/themes/default/single.php
+++ html/blog/wp-content/themes/default/single.php
@@ -7,7 +7,6 @@
 		<div class="post" id="post-<?php the_ID(); ?>">
         <p class="date"><small><?php the_time('F<\b\r>jS, Y') ?> <!-- by <?php the_author() ?> --></small></p>
 				<h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-        <br clear="all" />
 
 			<div class="entry">
 				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
