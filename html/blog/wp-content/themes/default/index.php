3378f18f4e6f14b5430804a596dd74ca74f0983b
Jaisen Mathai <jmathai@joythrown-lm-corp-yahoo-com.local>
2008-02-13 00:55:52 -0800

    Made tweaks to blog layout/graphics.

--- html/blog/wp-content/themes/default/index.php
+++ html/blog/wp-content/themes/default/index.php
@@ -7,10 +7,10 @@
 		<?php while (have_posts()) : the_post(); ?>
 
 			<div class="post" id="post-<?php the_ID(); ?>">
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-				<small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
-
-				<div class="entry">
+        <p class="date"><small><?php the_time('F<\b\r>jS, Y') ?> <!-- by <?php the_author() ?> --></small></p>
+				<h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+        <br clear="all" />
+        <div class="entry">
 					<?php the_content('Read the rest of this entry &raquo;'); ?>
 				</div>
 
--- html/blog/wp-content/themes/default/single.php
+++ html/blog/wp-content/themes/default/single.php
@@ -4,13 +4,10 @@
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<div class="navigation">
-			<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
-			<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
-		</div>
-
 		<div class="post" id="post-<?php the_ID(); ?>">
-			<h2><a href="<?php echo get_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+        <p class="date"><small><?php the_time('F<\b\r>jS, Y') ?> <!-- by <?php the_author() ?> --></small></p>
+				<h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+        <br clear="all" />
 
 			<div class="entry">
 				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
