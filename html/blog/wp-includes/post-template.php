3ce9ff800c1edc9e6fa8e2c642641149b7882841
Jaisen Mathai <jmathai@joythrown-lm.corp.yahoo.com>
2008-04-15 17:07:41 -0700

    Making updates to blog configs.

--- html/blog/wp-includes/post-template.php
+++ html/blog/wp-includes/post-template.php
@@ -76,19 +76,6 @@ function get_the_guid( $id = 0 ) {
 
 function the_content($more_link_text = '(more...)', $stripteaser = 0, $more_file = '') {
 	$content = get_the_content($more_link_text, $stripteaser, $more_file);
-  $content = str_replace('<!--more-->', '
-                <p id="blog-ads">
-                  <script type="text/javascript"><!--
-                  google_ad_client = "pub-5479658619507135";
-                  /* 468x60, created 2/28/08 */
-                  google_ad_slot = "9661027857";
-                  google_ad_width = 468;
-                  google_ad_height = 60;
-                  //-->
-                  </script>
-                  <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
-                </p>
-                ', $content);
 	$content = apply_filters('the_content', $content);
 	$content = str_replace(']]>', ']]&gt;', $content);
 	echo $content;
@@ -131,7 +118,18 @@ function get_the_content($more_link_text = '(more...)', $stripteaser = 0, $more_
 	$output .= $teaser;
 	if ( count($content) > 1 ) {
 		if ( $more ) {
-			$output .= '<span id="more-'.$id.'"></span>'.$content[1];
+			$output .= '<span id="more-'.$id.'"></span>
+                <p id="blog-ads">
+                  <script type="text/javascript"><!--
+                  google_ad_client = "pub-5479658619507135";
+                  /* 468x60, created 2/28/08 */
+                  google_ad_slot = "9661027857";
+                  google_ad_width = 468;
+                  google_ad_height = 60;
+                  //-->
+                  </script>
+                  <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+                </p>'.$content[1];
 		} else {
 			$output = balanceTags($output);
 			if ( ! empty($more_link_text) )
