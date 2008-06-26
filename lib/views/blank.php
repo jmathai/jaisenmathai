e0f5c7e53ce0f7e76e73bd449d2d53c97045cd7b
Jaisen Mathai <jaisen@jmathai.com>
2008-06-25 23:52:32 -0700

    Wordpress upgrade to 2.5.1.

---
+++ lib/views/blank.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+
+<head>
+  <style type="text/css">
+    @import url("/css/resume.css?1");
+  </style>
+  <title></title>
+</head>
+
+<body>
+  <div>
+    <?php
+      if(is_file($body))
+      {
+        //EpiCode::insert($body);
+        include $body;
+      }
+      else
+      {
+        echo $body;
+      }
+    ?>
+  </div>
+  
+  <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+  </script>
+  <script type="text/javascript">
+    _uacct = "UA-88708-4";
+    urchinTracker();
+  </script>
+</body>
+
+</html>
