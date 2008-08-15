024e7273dff640cc47d7adb45bd51457db1bb202
Jaisen Mathai <jaisen@jmathai.com>
2008-08-14 21:27:46 -0700

    Moving config.php inclusion into auto_prepend.php.

--- html/index.php
+++ html/index.php
@@ -1,5 +1,4 @@
 <?php
-  include_once './config.php';
   include_once PATH_LIB . '/EpiCode.php';
   
   if(EpiCode::getRoute($_GET['__route__'], $_['routes']) === false)
--- lib/scripts/auto_prepend.php
+++ lib/scripts/auto_prepend.php
@@ -1,4 +1,5 @@
 <?php
+  include_once(dirname(__FILE__) . '/../config.php');
   $memcache = null;
   function getMemcache()
   {
