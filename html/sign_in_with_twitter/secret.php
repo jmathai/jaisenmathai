319465aa18342ebbee348d117f63f21b8450475e
Jaisen Mathai <jaisen@jmathai.com>
2009-08-21 02:18:51 -0700

    Adding analytics to Twitter example and new alias for apache virtual host.

--- html/sign_in_with_twitter/secret.php
+++ html/sign_in_with_twitter/secret.php
@@ -1,4 +1,4 @@
 <?php
-$consumer_key = 'your_consumer_key';
-$consumer_secret = 'your_consumer_secret';
+$consumer_key = 'RWnvbFfdD5qbbk8hPhipRA';
+$consumer_secret = 'oyyOrzvRhlFAUABgxnWVBWo9Du15wCexET1AUC2Ztg';
 ?>
