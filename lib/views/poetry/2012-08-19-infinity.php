06ef8bb12916da06c95f8aaa896cc8db5d273be2
Jaisen Mathai <jaisen@jmathai.com>
2012-08-19 23:52:56 -0700

    Adding some limericks.

---
+++ lib/views/poetry/2012-08-19-infinity.php
@@ -0,0 +1,15 @@
+Infinity
+
+a regeneration of which i wasn't aware.
+my soul's been ripped from itself,
+and taken residence elsewhere.
+my existence resides no longer in me.
+any purpose i have is with another.
+the cavern measures to infinty,
+it's filled with fear and despair.
+and love.
+mainly love.
+you're clueless.
+despite my attempts at bringing you to understanding.
+you're two.
+this is just the beginning of what feels like forever.
